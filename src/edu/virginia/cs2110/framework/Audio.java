3fb51fa74d1fa13a3a75fcdb4d08d49407d7ec19
Jeffrey Cannon <jmc5fm@virginia.edu>
2013-11-12 06:42:37 -0500

    Deleted unused game frameworks

--- src/edu/virginia/cs2110/framework/Audio.java
+++
@@ -1,8 +0,0 @@
-package edu.virginia.cs2110.framework;
-
-	 public interface Audio {
-	     public Music createMusic(String file);
-
-	     public Sound createSound(String file);
-	 }
- 
--- src/edu/virginia/cs2110/framework/Sound.java
+++
@@ -1,7 +0,0 @@
-package edu.virginia.cs2110.framework;
-
-	public interface Sound {
-	    public void play(float volume);
-
-	    public void dispose();
-	}
