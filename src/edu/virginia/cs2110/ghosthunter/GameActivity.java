cd03512ed51b1a9ff0e54e8b382e7e696c3c926f
Jeffrey Cannon <jmc5fm@virginia.edu>
2013-11-17 00:01:54 -0500

    Tested some features of the map

--- src/edu/virginia/cs2110/ghosthunter/GameActivity.java
+++ src/edu/virginia/cs2110/ghosthunter/GameActivity.java
@@ -1,38 +1,12 @@
 package edu.virginia.cs2110.ghosthunter;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+public class GameActivity extends SingleFragmentActivity {
 
-public class GameActivity extends Activity {
+	@Override
+	protected Fragment createFragment() {
+		return new GameFragment();
+	}
 	
-	@TargetApi(11)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-        
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-	        // Get a handle to the Map Fragment
-	        GoogleMap map = ((MapFragment) getFragmentManager()
-	                .findFragmentById(R.id.map)).getMap();
-	
-	        LatLng sydney = new LatLng(-33.867, 151.206);
-	
-	        // map.setMyLocationEnabled(true);
-	        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-	
-	        map.addMarker(new MarkerOptions()
-	                .title("Sydney")
-	                .snippet("The most populous city in Australia.")
-	                .position(sydney));
-        }
-    }
 }
---
+++ src/edu/virginia/cs2110/ghosthunter/GameFragment.java
@@ -0,0 +1,55 @@
+package edu.virginia.cs2110.ghosthunter;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GameFragment extends Fragment {
+	
+	public static final int ZOOM_FACTOR = 18;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_game, parent, false);
+
+		GoogleMap map = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		// map.setMyLocationEnabled(true);
+		
+		LocationManager lm = (LocationManager) getActivity().getSystemService(GameActivity.LOCATION_SERVICE);
+		Location currentLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		
+		LatLng mapCenter;
+		
+		if (currentLocation != null) {
+			mapCenter = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+		} else {
+			mapCenter = new LatLng(38.0299, -78.4790);
+		}
+		
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, ZOOM_FACTOR));
+		
+		// Flat markers will rotate when the map is rotated,
+		// and change perspective when the map is tilted.
+		map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.ghost_icon)).position(mapCenter).flat(true).rotation(245));
+
+		CameraPosition cameraPosition = CameraPosition.builder().target(mapCenter).zoom(13).bearing(90).build();
+	        
+		// Animate the change in camera view over 2 seconds
+		map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 2000, null);
+		
+		return v;
+	}
+	
+}
